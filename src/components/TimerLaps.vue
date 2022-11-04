efdab8488d9f3789ff02baa1f6bf7573fc09eaeb
Tia <nik12346@yandex.ru>
2022-11-04 14:25:22 +0300

    add transitions

--- src/components/TimerLaps.vue
+++ src/components/TimerLaps.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="laps">
-    <div
-        v-for="lap in laps.slice().reverse()"
-        :key="lap.id"
-        class="lap"><span>{{lap.id}}: <my-clock :time="lap.lapTime"/></span> <span style="color: gray">
-            +<my-clock :time="lap.delta"/>
-        </span></div>
+    <transition-group name="lap-list">
+        <div
+            v-for="lap in laps.slice().reverse()"
+            :key="lap.id"
+            class="lap"><span>{{lap.id}}: <my-clock :time="lap.lapTime"/></span> <span style="color: gray">
+                +<my-clock :time="lap.delta"/>
+            </span></div>
+    </transition-group>
     </div>
 </template>
 
@@ -32,5 +34,19 @@
   font-family: Roboto;
   display: flex;
   justify-content: space-between;
+}
+
+.lap-list-enter-active,
+.lap-list-enter-active{
+    transition: all .2s ease;
+}
+
+.lap-list-enter-from{
+    opacity: 0;
+    transform: translateY(-20px);
+}
+
+.lap-list-move{
+    transition: all 0.2s ease;
 }
 </style>
